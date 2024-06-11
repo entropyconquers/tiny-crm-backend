5dc71962039b64ab301c54f30a2f6e0665a6f8d4
entropyconquers <breakingcortana5@gmail.com>
2024-06-12 00:48:32 +0530

    chore: Update server.js with CORS and cookie session configurations

--- server.js
+++ server.js
@@ -12,19 +12,27 @@ const cors = require('cors'); // Import cors
 const isLoggedIn = require('./middleware/auth');
 
 const app = express();
+
 app.use(cors({
     origin: '*', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
     credentials: true, // Include credentials in the CORS requests
     preflightContinue: false,
-    optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204,
+    credentials: true,
   }));
-
+  app.set('trust proxy', 1)  // you need to add this
 
 app.use(cookieSession({
   name: 'google-auth-session',
-  keys: ['key1', 'key2']
+  keys: ['key1', 'key2'],
+  proxy: true, // Optional, depends on the server you're hosting on. For Heroku, you might need it.
+  cookie: {
+    secure: process.env.NODE_ENV === "production", // Automatically set to true in production and false in development
+    maxAge: 10000, // 10 seconds for testing
+    sameSite: "none", // In development, this will default to 'lax'. Set to 'none' for cross-site requests.
+  }
 }))
 
 app.use(passport.initialize());
